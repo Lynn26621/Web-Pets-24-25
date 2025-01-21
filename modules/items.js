b6ce01ef88c2e89841be8cd42e6b118978232dc6
kris26658 <129105342+kris26658@users.noreply.github.com>
2025-01-21 14:42:22 -0500

    Items and Inventory?
    Non-functional spaghetti code-looking nonsense. I'll fix it at some point.

---
+++ modules/items.js
@@ -0,0 +1,5 @@
+const classList = [foods, toys, costumes]
+
+const foods = ["apple", "banana", "meat", "candy"];
+const toys = ["rubber ball", "yarn ball", "chew toy", "feather toy"];
+const costumes = ["sweater", "tutu", "ball cap"];
